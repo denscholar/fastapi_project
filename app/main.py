23d8cc89ba7b9c199011edd1e02f600cf40bfe40
Dennis Akagha <dennis.akagha@afripointgroup.com>
2025-12-13 02:20:40 +0100

    add to code.

--- app/main.py
+++ app/main.py
@@ -14,6 +14,12 @@ class PostModel(BaseModel):
     rating: Optional[int] = None
 
 
+class UpdatePost(BaseModel):
+    title: str
+    content: str
+    rating: Optional[int] = None
+
+
 my_posts = [
     {
         "id": 1,
@@ -61,11 +67,22 @@ async def get_post(id: int) -> dict:
     raise HTTPException(status_code=404, detail=f"post with id: {id} was not found")
 
 
-@app.put("/posts/{id}")
-async def update_post(id: int, updated_post: PostModel, response: Response) -> dict:
+@app.put("/posts/{id}", status_code=status.HTTP_200_OK)
+async def update_post(id: int, updated_post: UpdatePost) -> dict:
     for index, post in enumerate(my_posts):
         if post["id"] == id:
             my_posts[index] = updated_post.model_dump()
             my_posts[index]["id"] = id
-            return {"message": "post updated successfully", "data": my_posts[index]}
-        response.status_code = 404
+            return Response(
+                content=f'{{"message": "post updated successfully", "data": {my_posts[index]}}}'
+            )
+        raise HTTPException(status_code=404, detail=f"post with id: {id} was not found")
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int) -> Response:
+    for index, post in enumerate(my_posts):
+        if post["id"] == id:
+            my_posts.pop(index)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status_code=404, detail=f"post with id: {id} was not found")
